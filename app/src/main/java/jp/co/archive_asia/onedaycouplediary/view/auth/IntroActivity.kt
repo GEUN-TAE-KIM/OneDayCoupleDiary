cd08a5b76fd7c8b31eb4caa61d844e5734d65773
kimgeuntae <rmsxo5678@naver.com>
2022-08-18 21:28:15 +0900

    edit commit

--- app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/auth/IntroActivity.kt
+++ app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/auth/IntroActivity.kt
@@ -8,7 +8,7 @@ import jp.co.archive_asia.onedaycouplediary.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityIntroBinding
+    private lateinit var binding: ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,17 +18,17 @@ class IntroActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.loginBtn.setOnClickListener{
+        binding.loginBtn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        binding.joinBtn.setOnClickListener{
+        binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
         }
 
-        binding.noAccountBtn.setOnClickListener{
+        binding.noAccountBtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/fragment/CalendarFragment.kt
+++ app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/fragment/CalendarFragment.kt
@@ -5,15 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import jp.co.archive_asia.onedaycouplediary.R
+import jp.co.archive_asia.onedaycouplediary.databinding.FragmentCalendarBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class CalendarFragment : Fragment() {
+
+    private lateinit var binding: FragmentCalendarBinding
+    lateinit var selectedDate: LocalDate
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calendar, container, false)
+
+        selectedDate = LocalDate.now()
+
+        setMonthView()
+
+        binding.preBtn.setOnClickListener {
+            selectedDate = selectedDate.minusMonths(1)
+            setMonthView()
+        }
+
+        binding.nextBtn.setOnClickListener {
+            selectedDate = selectedDate.plusMonths(1)
+            setMonthView()
+        }
+
+        return binding.root
+    }
+
+    private fun setMonthView() {
+        binding.monthYearText.text = monthYearFromDate(selectedDate)
+    }
+
+    private fun monthYearFromDate(data: LocalDate): String {
+
+        var formatter = DateTimeFormatter.ofPattern("yyyy MM月")
+
+        return data.format(formatter)
     }
 
 }
