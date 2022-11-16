9f447153107d4193756110a0d6ebf70688e14cff
rmsxo5678 <rmsxo5678@naver.com>
2022-11-16 15:38:11 +0900

    Firebase Join Login  連動

--- app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/auth/JoinFragment.kt
+++ app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/auth/JoinFragment.kt
@@ -1,16 +1,126 @@
 package jp.co.archive_asia.onedaycouplediary.view.auth
 
+import android.content.Intent
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import jp.co.archive_asia.onedaycouplediary.MainActivity
 import jp.co.archive_asia.onedaycouplediary.R
 import jp.co.archive_asia.onedaycouplediary.databinding.FragmentJoinBinding
 import jp.co.archive_asia.onedaycouplediary.view.BaseFragment
 
+
 class JoinFragment : BaseFragment<FragmentJoinBinding>(R.layout.fragment_join) {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun initView() {
         super.initView()
+
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
         binding.btnJoin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            createAccount(email, password)
+
             findNavController().navigate(R.id.action_joinFragment_to_calendarFragment)
         }
+
+        auth = Firebase.auth
     }
+
+
+    // 사용자가 있는지 없는지 확인
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload()
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "성공")
+                    val user = auth.currentUser
+                    updateUI(user)
+
+                } else {
+                    Log.w(TAG, "실패", task.exception)
+                    Toast.makeText(context, "올바르게 입력 해야 함", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+            }
+    }
+
+    private fun reload() {
+        auth.currentUser!!.reload().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Reload successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(TAG, "reload", task.exception)
+                Toast.makeText(context, "Failed to reload user.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.etEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.etPassword.error = "Required."
+            valid = false
+        } else {
+            binding.etPassword.error = null
+        }
+
+        val password = binding.etPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.etPassword.error = "Required."
+            valid = false
+        } else {
+            binding.etPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+
+
+            if (user.isEmailVerified) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
 }
--- app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/auth/LoginFragment.kt
+++ app/src/main/java/jp/co/archive_asia/onedaycouplediary/view/auth/LoginFragment.kt
@@ -1,20 +1,115 @@
 package jp.co.archive_asia.onedaycouplediary.view.auth
 
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthMultiFactorException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import jp.co.archive_asia.onedaycouplediary.R
 import jp.co.archive_asia.onedaycouplediary.databinding.FragmentLoginBinding
 import jp.co.archive_asia.onedaycouplediary.view.BaseFragment
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun initView() {
+
         super.initView()
+
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+
         binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            signIn(email, password)
+
             findNavController().navigate(R.id.action_loginFragment_to_calendarFragment)
+
+        }
+
+        auth = Firebase.auth
+
+    }
+
+    // 사용자가 있는지 없는지 확인
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload()
         }
     }
 
+    private fun signIn(email: String, password: String) {
+
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d("로그인", "성공")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    updateUI(null)
+                    Log.d("로그인", "실패")
+                }
+
+            }
+    }
+
+    private fun reload() {
+        auth.currentUser!!.reload().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+
+                Toast.makeText(context, "Reload successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(TAG, "reload", task.exception)
+                Toast.makeText(context, "Failed to reload user.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.etEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.etEmail.error = "Required."
+            valid = false
+        } else {
+            binding.etEmail.error = null
+        }
+
+        val password = binding.etPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.etPassword.error = "Required."
+            valid = false
+        } else {
+            binding.etPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
 }
